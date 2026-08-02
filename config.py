ddf1d3be7cda2d04c96fc8cc4850ab04275600e4
wams77 <77565674+wams77@users.noreply.github.com>
2026-08-03 07:42:31 +0800

    Add configuration for environment variables

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+FACEBOOK_PAGE_ID = os.getenv("FACEBOOK_PAGE_ID")
+FACEBOOK_ACCESS_TOKEN = os.getenv("FACEBOOK_ACCESS_TOKEN")
